44ddc74caa8edabe721a7be669f706141f155d77
CharlotteMD <charlottemdavies@hotmail.co.uk>
2019-08-12 15:21:36 +0100

    three boxes all spinning in one go

--- hello-cube/js/main.js
+++ hello-cube/js/main.js
@@ -11,11 +11,16 @@ function main() {
 
     // you can also render directly to the dom via js
 
-    var renderer = new THREE.WebGLRenderer();
+    const renderer = new THREE.WebGLRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
 
-    var scene = new THREE.Scene();
+    const fov = 75; // the angle from the eye to the object (in degrees) (the smaller the number the bigger the cube/closer to the eye)
+    const aspect = 2;  // the canvas default - size of the display/ratio of the display
+    const near = 0.1; // the distance from the front of the camera where the rendering will start
+    const far = 5; // the distance from the front of the camera where the rendering will end
+
+    const scene = new THREE.Scene();
 
     // here's the lighting! this makes each side of the cube a slightly different shade
     {
@@ -25,11 +30,6 @@ function main() {
         light.position.set(-1, 2, 4);
         scene.add(light);
       }
-
-    const fov = 75; // the angle from the eye to the object (in degrees) 
-    const aspect = 2;  // the canvas default - size of the display/ratio of the display
-    const near = 0.1; // the distance from the front of the camera where the rendering will start
-    const far = 5; // the distance from the front of the camera where the rendering will end
 
     const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
     camera.position.z = 2; // camera position '2' back from the origin so you can take in the whole image.
@@ -43,26 +43,41 @@ function main() {
     const boxDepth = 1;
     const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
 
-    // color and material of the object - color hexcode starts with 0x
-    // const material = new THREE.MeshBasicMaterial({color: 0xee82ee});
-    // the MeshBasicMaterial doesnt work with light so use:
-    const material = new THREE.MeshPhongMaterial({color: 0xee82ee});  
+    // make additional cubes
+    function makeInstance(geometry, color, x) {
+           
+        // color and material of the object - color hexcode starts with 0x
+        // const material = new THREE.MeshBasicMaterial({color: 0xee82ee});
+        // the MeshBasicMaterial doesnt work with light so use:
+        const material = new THREE.MeshPhongMaterial({color});
+       
+        // puts the size and color together
+        const cube = new THREE.Mesh(geometry, material);
 
-    // puts the size and color together
-    const cube = new THREE.Mesh(geometry, material);
+        // add the cube to the scene and render the scene and camera
+        scene.add(cube);
+       
+        cube.position.x = x;
+       
+        return cube;
+      }
 
-    // add the cube to the scene and render the scene and camera
-    scene.add(cube);
-
-    // when you add animation this moves to the animation
-    // renderer.render(scene, camera);
+      const cubes = [
+        makeInstance(geometry, 0xee82ee,  0),
+        makeInstance(geometry, 0x8844aa, -2),
+        makeInstance(geometry, 0x00bbff,  2),
+      ];
 
     // and here's the animation!!! :-D
     function render(time) {
         time *= 0.001;  // convert time to seconds
        
-        cube.rotation.x = time;
-        cube.rotation.y = time;
+        cubes.forEach((cube, ndx) => {
+            const speed = 1 + ndx * .1;
+            const rot = time * speed;
+            cube.rotation.x = rot;
+            cube.rotation.y = rot;
+          });
        
         // this is three.js' function that draws the object
         renderer.render(scene, camera);
