0e390ccd84303d41bc4c56225c0b4d4275afe22e
CharlotteMD <charlottemdavies@hotmail.co.uk>
2019-08-12 11:49:25 +0100

    rendering via js

--- hello-cube/js/main.js
+++ hello-cube/js/main.js
@@ -1,32 +1,41 @@
+console.log('go');
+
 'use strict';
 
 // /* global THREE */
 
 function main() {
-    const canvas = document.querySelector('#c');
-    const renderer = new THREE.WebGLRenderer({canvas});
+    // you can also render with a canvas included as an element in the html
+    // const canvas = document.querySelector('#c');
+    // const renderer = new THREE.WebGLRenderer({canvas});
+
+    // you can also render directly to the dom via js
+
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+
+    var scene = new THREE.Scene();
 
     const fov = 75; // the angle from the eye to the object (in degrees) 
     const aspect = 2;  // the canvas default - size of the display/ratio of the display
     const near = 0.1; // the distance from the front of the camera where the rendering will start
     const far = 5; // the distance from the front of the camera where the rendering will end
-    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 
+    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
     camera.position.z = 2; // camera position '2' back from the origin so you can take in the whole image.
 
     // camera --> origin --> near point (start rendering) --> object --> far point (stop rendering)
     // the aspect calculates the width of the object based on the fov degrees from the origin - starts at the origin and goes outwards like a triangle
 
-    const scene = new THREE.Scene();
-
     // size and dimensions of the object
-    const boxWidth = 1;
+    const boxWidth = 2;
     const boxHeight = 1;
     const boxDepth = 1;
     const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
 
-    // color and material of the object
-    const material = new THREE.MeshBasicMaterial({color: 0x44aa88});
+    // color and material of the object - color hexcode starts with 0x
+    const material = new THREE.MeshBasicMaterial({color: 0xee82ee});
 
     // puts the size and color together
     const cube = new THREE.Mesh(geometry, material);
